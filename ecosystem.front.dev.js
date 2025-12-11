ccadc61f3cdb9ab5e5940b5e36a991074b0bf441
millerbermeo <millerbermeo30@gmail.com>
2025-12-10 22:48:17 -0500

    commit opuertos

--- ecosystem.front.dev.js
+++ ecosystem.front.dev.js
@@ -3,7 +3,7 @@ module.exports = {
     {
       name: "react-dev",
       script: "npm",
-      args: "run dev -- --host 0.0.0.0 --port 5174",
+      args: "run dev -- --host 0.0.0.0 --port 5175",
       env: {
         NODE_ENV: "development"
       }
