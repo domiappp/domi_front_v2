77cb8fad53bf673c5a7342481f2827956d0e2198
millerbermeo <millerbermeo30@gmail.com>
2025-12-10 22:55:58 -0500

    correccion 123

--- ecosystem.front.dev.cjs
+++ ecosystem.front.dev.cjs
@@ -1,12 +1,12 @@
-export default {
+module.exports = {
   apps: [
     {
       name: "react-dev",
       script: "npm",
       args: "run dev -- --host 0.0.0.0 --port 5175",
       env: {
-        NODE_ENV: "development"
-      }
-    }
-  ]
+        NODE_ENV: "development",
+      },
+    },
+  ],
 };
